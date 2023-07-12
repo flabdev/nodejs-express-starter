cae3e4043460c2df161a30e56392c7e2753eedad
saikrishna <saikrishna.tatipamula@fissionlabs.com>
2023-07-12 18:15:40 +0530

    error handler added

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,10 +10,29 @@ const sendErrorDev = (err, req, res) => {
       stack: err.stack,
     });
   }
-  console.log('ERROR', err);
+  console.error('ERROR ', err);
   return res.status(err.statusCode).render('error', {
     title: 'Something went wrong!',
     msg: err.message,
+  });
+};
+
+const sendErrorProd = (err, req, res) => {
+  if (req.originalUrl.startsWith('/api')) {
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+    return res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: 'Please try again later.',
   });
 };
 
@@ -21,8 +40,10 @@ module.exports = (err, req, res) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (config.NODE_ENV === 'production') {
+  if (config.NODE_ENV === 'development') {
+    sendErrorDev(err, req, res);
+  } else {
     utils.writeErrorLog(err, req);
+    sendErrorProd(err, req, res);
   }
-  sendErrorDev(err, req, res);
 };
